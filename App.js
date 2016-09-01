3bfa738ad6d8eb2b5406a4534a724c0b8318b136
Jun Kai Lim <junkaiii@gmail.com>
2016-09-02 01:24:25 +0800

    seperated two textboxes by using refs

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      value: 0,
-      multiplier: ""
+      value: "",
+      multiplier: "",
+      result: ""
     }
     this.update=this.update.bind(this)
   }
@@ -14,15 +15,18 @@ class App extends React.Component {
   update(event){
     let input = event.target.value
     this.setState({
-      multiplier: input
+      multiplier: ReactDOM.findDOMNode(this.refs.multiplier).value,
+      value: ReactDOM.findDOMNode(this.refs.value).value
     })
   }
 
   render(){
     return (
       <div>
-      <input type="text" onChange={this.update} value={this.state.multiplier}></input>
+      <input type="text" ref="multiplier" className="multiplier" onChange={this.update} value={this.state.multiplier}></input>
       <p>{this.state.multiplier}</p>
+      <input type="text" ref="value" className="value" onChange={this.update} value={this.state.value}></input>
+      <p>{this.state.value}</p>
       </div>
     )
   }
