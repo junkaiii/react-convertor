805111a694455a4887c26c9eeaf8e94a7ad79d1d
Jun Kai Lim <junkaiii@gmail.com>
2016-09-02 10:24:15 +0800

    removed verbose codes

--- App.js
+++ App.js
@@ -13,22 +13,11 @@ class App extends React.Component {
   }
 
   update(event){
-    let input = event.target.value
     this.setState({
-    //  result: ReactDOM.findDOMNode(this.refs.multiplier).value * ReactDOM.findDOMNode(this.refs.value).value
       result: this._mul.value * this._val.value
     })
   }
 
-  // render(){
-  //   return (
-  //     <div>
-  //     <input type="text" ref="multiplier" className="multiplier" onChange={this.update}></input>
-  //     <input type="text" ref="value" className="value" onChange={this.update} /></input>
-  //     <p>{this.state.result}</p>
-  //     </div>
-  //   )
-  // }
 
   render(){
     return (
